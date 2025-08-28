c25d8509e9a3ca2fde1bfea9bbf6b9967577cbe7
Yuge Zhang <scottyugochang@gmail.com>
2025-08-28 21:20:57 +0800

    .

--- packages/poml-browser/ui/registry.ts
+++ packages/poml-browser/ui/registry.ts
@@ -1,8 +1,10 @@
 /* To trigger the registered functions in the RPC */
 
-import { pingPong } from '../common/rpc';
+import { pingPong } from '@common/rpc';
+import { readFile } from '@common/imports/file';
 
 // @ts-ignore
 if (__TEST_BUILD__) {
+  (window as any).readFile = readFile;
   (window as any).pingPong = pingPong; // Expose pingPong for testing
 }
